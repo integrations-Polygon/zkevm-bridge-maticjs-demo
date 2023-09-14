7235f12c2a3c3e760269dbc08f747f3149906dfb
Gulam Rasul Shah <99994920+grsLammy@users.noreply.github.com>
2023-09-14 09:23:47 +0300

    feat: add root and child token smart contracts

---
+++ src/goerli/RootToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RootToken is Ownable, ERC20 {
+    uint256 constant INITIAL_SUPPLY = 100000000 * (1e18);
+
+    constructor() ERC20("Test ERC20 Token", "TestERC20Token") {
+        _mint(_msgSender(), INITIAL_SUPPLY);
+    }
+}
