d373015f708b1f76254899bc732fd6fdfc0fb41b
Gulam Rasul Shah <99994920+grsLammy@users.noreply.github.com>
2023-09-15 14:31:41 +0300

    feat: update root token smart contract

--- src/ERC20TestToken.sol
+++ src/ERC20TestToken.sol
@@ -4,10 +4,10 @@ pragma solidity 0.8.21;
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RootToken is Ownable, ERC20 {
+contract ERC20TestToken is Ownable, ERC20 {
     uint256 constant INITIAL_SUPPLY = 100000000 * (1e18);
 
-    constructor() ERC20("Test ERC20 Token", "TestERC20Token") {
+    constructor() ERC20("ERC20 Test Token", "ETT") {
         _mint(_msgSender(), INITIAL_SUPPLY);
     }
 }
