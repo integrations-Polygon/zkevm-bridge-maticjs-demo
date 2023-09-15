aca1040207192c7ab16ab1db2fec9e49ca2517ff
Gulam Rasul Shah <99994920+grsLammy@users.noreply.github.com>
2023-09-15 14:31:27 +0300

    feat: update deploy script and add Programmatic verification

--- scripts/0_deploy/deploy_rootToken.ts
+++ scripts/0_deploy/deploy_rootToken.ts
@@ -1,16 +1,29 @@
-import { ethers } from "hardhat";
-import { sleep } from "../utils/sleep";
-import { RootToken__factory } from "../../src/types";
+import { ethers, run } from "hardhat";
+import { sleep } from "../utils/utilityFunctions";
+import { ERC20TestToken__factory } from "../../src/types";
 
 async function deployRootToken() {
   // get the contract to deploy
-  const RootToken = (await ethers.getContractFactory("RootToken")) as RootToken__factory;
+  const RootToken = (await ethers.getContractFactory("ERC20TestToken")) as ERC20TestToken__factory;
   const rootToken = await RootToken.deploy();
+  console.clear();
   console.log("\nDeploying Root Token on Goerli chain....");
 
-  await sleep(20000);
-  console.log("\nRoot Token contract deployed at: ", rootToken.address);
-  console.log(`Contract Details: https://goerli.etherscan.io/address/${rootToken.address}`);
+  await sleep(60000); // wait at least 60sec for state change
+  console.log(`Root Token contract deployed at: ${rootToken.address}\n\n`);
+
+  /**
+   * Programmatic verification
+   */
+  try {
+    await run("verify:verify", {
+      address: rootToken.address,
+      contract: "src/ERC20TestToken.sol:ERC20TestToken",
+      constructorArguments: [],
+    });
+  } catch (error: any) {
+    throw new Error(`error in verifying: ${error.message}`);
+  }
 }
 
 deployRootToken();
